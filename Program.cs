1c21e2ab9295611f474f7b21790d80480529123e
Bernhard Richter <bernhardrichter@lro-2570p.dips.local>
2015-10-20 09:47:35 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.AspNet.SignalR;
+using Owin;
+using Microsoft.Owin.Hosting;
+
+namespace TestSignalR
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			WebApp.Start<Startup>("localhost:5001");
+			Console.WriteLine ("Hello World!");
+			Console.ReadLine ();
+		}
+	}
+
+
+	public class MainHub : Hub
+	{
+		public void Send(string name, string message)
+		{
+			// Call the broadcastMessage method to update clients.
+			Clients.All.broadcastMessage(name, message);
+
+		}
+	}
+
+
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			app.MapSignalR();
+		}
+
+
+	}
+
+}
